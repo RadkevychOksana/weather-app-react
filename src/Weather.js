26d33c50ac8a37db3f956f70b06f920afa74a8cb
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-07-26 16:09:53 +0200

    Removed spinner

--- src/Weather.js
+++ src/Weather.js
@@ -4,20 +4,14 @@ import { Grid } from  'react-loader-spinner';
 
 export default function Weather(props){
     function showTemp(response){
-        let string = `The temperature in ${response.data.name} is ${Math.round(response.data.main.temp)}`;
-        return string;
+        alert(`The temperature is ${response.data.main.temp}`);
     }
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
     axios(apiUrl).then(showTemp);
 
     return( 
     <div>
-       
-        <h1>Herzlich Willkommen in React!</h1><Grid
-        height = "80"
-        width = "80"
-        color = '#61dafb'
-      /> 
+        <h1>Herzlich Willkommen in React!</h1>
     </div>
   );  
 }
