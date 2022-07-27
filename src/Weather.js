382610c2a784604a64e417ae2186b5e4fe8488db
Radkevych Oksana <radkevych.oksana@gmail.com>
2022-07-27 20:02:44 +0200

    Added data API
    1. Temperature
    2. Wind Speed
    3. Humidity
    4. Description
    5. Pressure

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,26 @@
-import React from "react";
-//import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 //import { Grid } from  'react-loader-spinner';
+import "bootstrap/dist/css/bootstrap.css";
 
-export default function Weather(props){
-    return(
-        <div className="container">
+export default function Weather(props) {
+  const [weatherData,setWeatherData] = useState({ready: false});
+  function showTemp(response) {
+    setWeatherData({
+      ready:true,
+      temp: Math.round(response.data.main.temp),
+      city: response.data.name,
+      windSpeed: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl:"https://st.depositphotos.com/1007168/1249/i/600/depositphotos_12492703-stock-photo-summer-hot-sun.jpg",
+      description: response.data.weather[0].description,
+      pressure: response.data.main.pressure,
+      date: "Tuesday 10:00"
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="container">
         <div className="weather-app-wrapper">
           <div className="weather-app shadow p-3 mb-5 bg-body rounded">
             <form id="search-form" className="mb-3">
@@ -29,26 +45,25 @@ export default function Weather(props){
             </form>
             <div className="overview">
               <h1 id="city" className="fs-1 fw-bold text-info">
-                London
+                {weatherData.city}
               </h1>
               <ul>
                 <li>
-                  Last updated: <span id="date">Tuesday 10:00</span>
+                  Last updated: <span>{weatherData.date}</span>
                 </li>
-                <li id="description">Clear</li>
+                <li className="text-capitalize">{weatherData.description}</li>
               </ul>
             </div>
             <div className="row">
               <div className="col-6">
                 <div className="clearfix weather-temperature">
                   <img
-                    src="https://st.depositphotos.com/1007168/1249/i/600/depositphotos_12492703-stock-photo-summer-hot-sun.jpg"
+                    src={weatherData.iconUrl}
                     alt="Clear"
-                    id="icon"
                     className="float-left"
                   />
                   <div className="float-left">
-                    <strong id="temperature">25</strong>
+                    <strong id="temperature">{weatherData.temp}</strong>
                     <span className="units"> °C</span>
                   </div>
                 </div>
@@ -56,10 +71,13 @@ export default function Weather(props){
               <div className="col-6">
                 <ul>
                   <li>
-                    Humidity: <span id="humidity">80 </span>%
+                    Pressure: <span>{weatherData.pressure}</span>
                   </li>
                   <li>
-                    Wind: <span id="wind"></span> 10 m/h
+                    Humidity: <span> {weatherData.humidity} </span>%
+                  </li>
+                  <li>
+                    Wind: <span></span> {Math.round(weatherData.windSpeed)} m/h
                   </li>
                 </ul>
               </div>
@@ -69,4 +87,10 @@ export default function Weather(props){
         </div>
       </div>
     );
+  } else {
+    let apiKey = "149e1223e69e53cd644a15607bc75a82";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios(apiUrl).then(showTemp);
+    return "loading...";
+  }
 }
